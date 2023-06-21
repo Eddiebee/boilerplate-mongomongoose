f36667c534b578987310c103679eb2cd7c3bc69c
eddieabasi <edbeeotudor@gmail.com>
2023-06-21 06:45:48 +0000

    add classic updates by using find, edit, then save

--- myApp.js
+++ myApp.js
@@ -65,8 +65,14 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return done(err);
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, editedPerson) => {
+      if (err) return done(err);
+      done(null, editedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
